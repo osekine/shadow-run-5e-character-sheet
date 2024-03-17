d57466f2f5363cc91d6b4941acac41123e950164
neonLaserUnicorn <astrargot@gmail.com>
2024-03-17 18:19:28 +0300

    init commit

---
+++ android/app/src/main/kotlin/com/example/shadowrun_5e_character_sheet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shadowrun_5e_character_sheet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/model/weapon_model.dart
@@ -0,0 +1,12 @@
+enum Firemode { ss, sa, fa }
+
+class WeaponModel {
+  String? name;
+  String? type;
+  int? accuracy;
+  int? damage;
+  int? AP;
+  int? magazine;
+  Firemode? firemode;
+  int? recoil;
+}

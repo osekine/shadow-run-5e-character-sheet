23088e10651eebbd3656b670e96e53d8967e600b
NeonLaserUnicorn <astrargot@gmail.com>
2024-03-20 14:59:48 +0300

    create text utility file

---
+++ lib/utility/text_widgets.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  final String _text;
+  final Color _color;
+  const BigText({super.key, required String text, required Color color}) : _text = text, _color = color;
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}

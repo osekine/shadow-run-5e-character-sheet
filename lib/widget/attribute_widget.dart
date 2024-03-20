3b81ecdf4d6e412f22b89e0440d330112da76202
NeonLaserUnicorn <astrargot@gmail.com>
2024-03-20 14:20:40 +0300

    add separate HealthModel

--- lib/widget/attribute_widget.dart
+++ lib/widget/attribute_widget.dart
@@ -7,31 +7,37 @@ class AttributeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ClipRRect(
-          borderRadius: BorderRadius.circular(13),
-          child: IntrinsicHeight(
-            child: Card(
-              color: Color(0xD9D9D9),
-              elevation: 4.0,
-              child: SizedBox(
-                height: 110,
-                width: 90,
-      
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(padding: const EdgeInsets.all(5), child: SizedBox(width: 80, height: 80, child: ClipRRect(borderRadius: BorderRadius.circular(8), child: Card(color: Color(0xFFFAFA), child: Center(child: Text('$value')),))),
-                      ),
-                      Text(name),
-                      
-                    ]
-                  ),
-              
-              ),
+        borderRadius: BorderRadius.circular(13),
+        child: IntrinsicHeight(
+          child: Card(
+            color: Color.fromARGB(169, 217, 217, 217),
+            elevation: 4.0,
+            child: SizedBox(
+              height: 110,
+              width: 90,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: SizedBox(
+                          width: 80,
+                          height: 80,
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Card(
+                                color: Color.fromARGB(255, 255, 250, 250),
+                                child: Center(child: Text(name == 'Ent' ? '${value/100}${value%100}' : '$value')),
+                              ))),
+                    ),
+                    Text(name),
+                  ]),
             ),
           ),
+        ),
       ),
     );
   }

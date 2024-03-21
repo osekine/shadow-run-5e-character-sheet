144b1c4b38946cc655c9de9d1355c284800dec89
NeonLaserUnicorn <astrargot@gmail.com>
2024-03-21 13:03:16 +0300

    create second row in WeaponWidget

--- lib/widget/weapon_widget.dart
+++ lib/widget/weapon_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shadowrun_5e_character_sheet/model/weapon_model.dart';
@@ -10,19 +12,20 @@ class WeaponWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return 
-           Padding(
-             padding: const EdgeInsets.all(25.0),
-            child: IntrinsicHeight(
-              child: SizedBox(
-                width: constraints.maxWidth,
-                child: Column(
+    return LayoutBuilder(builder: (context, constraints) {
+      return Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Container(
+          color: Colors.grey,
+          child: IntrinsicHeight(
+            child: SizedBox(
+              width: constraints.maxWidth,
+              child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Flex(
-                      direction: Axis.horizontal,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      // direction: Axis.horizontal,
                       children: [
                         DataCell(title: 'Name', data: model.name!, flex: 6),
                         DataCell(title: 'Acc', data: model.accuracy!, flex: 1),
@@ -30,32 +33,75 @@ class WeaponWidget extends StatelessWidget {
                         DataCell(title: 'AP', data: model.AP!, flex: 1),
                         DataCell(title: 'Rec', data: model.recoil!, flex: 1),
                       ],
-                    )
-                  ]
-                           ),
-              ),
+                    ),
+                    Flex(direction: Axis.horizontal, children: [
+                      Flexible(
+                        flex: 2,
+                        fit: FlexFit.tight,
+                        child: Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: Row(children: [
+                            SmallText(text: 'Firemode'),
+                            ...model.firemode.map((e) => FiremodeCell(e!)),
+                          ]),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SmallText(text: 'Ammo'),
+                              SizedBox(width: 5),
+                              Container(
+                                color: Colors.white,
+                                child: SmallText(
+                                    text: '${model.magazine}/${model.magazine}'),
+                              )
+                            ]),
+                      ),
+                    ])
+                  ]),
             ),
-           );
-      }
-        );
-      
+          ),
+        ),
+      );
+    });
   }
 
   Widget DataCell(
       {required int flex, required String title, required dynamic data}) {
-    return 
-      
-         Flexible(
-            fit: FlexFit.tight,
-            flex: flex,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SmallText(text: title),
-                Container(color: Colors.white, child: SmallText(text:data.toString())),
-              ],
-            ),
-  
+    return Flexible(
+      fit: FlexFit.tight,
+      flex: flex,
+      child: Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SmallText(text: title),
+            Container(
+                color: Colors.white, child: SmallText(text: data.toString())),
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget FiremodeCell(Firemode firemode) {
+  return Container(
+    child: SmallText(text: firemode.toString()),
+    width: 18,
+    height: 18,
+    decoration: BoxDecoration(
+      color: Color.fromARGB(
+        255,
+        Random().nextInt(255),
+        Random().nextInt(255),
+        Random().nextInt(255),
+      ),
+      shape: BoxShape.circle,
+    ),
+  );
 }

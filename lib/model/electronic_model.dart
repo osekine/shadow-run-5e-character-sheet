530e9e2e685e5d3bad0619e0896accd9f59534f4
NeonLaserUnicorn <astrargot@gmail.com>
2024-03-22 16:06:54 +0300

    added maps with commlinks and decks; added two factory constructors for Deck

--- lib/model/electronic_model.dart
+++ lib/model/electronic_model.dart
@@ -18,9 +18,26 @@ class Commlink extends ElectronicModel {
                 dataProc: rating,
                 firewall: rating));
 }
+Map<String, ElectronicModel> commlinks = {
+  'Metalink': Commlink(name: 'Metalink', rating: 1),
+  'Sony': Commlink(name: 'Sony Emperor', rating: 2),
+  'Renraku': Commlink(name: 'Renraku Sensei', rating: 3),
+  'Erica': Commlink(name: 'Erica Elite', rating: 4),
+  'Hermes': Commlink(name: 'Hermes Icon', rating: 5),
+  'Transis': Commlink(name: 'Transis Avalon', rating: 6),
+  'Firelight': Commlink(name: 'Firelight', rating: 7),
+};
 
+class DeckApplication{
+  final String name;
+  final String description;
+
+  const DeckApplication({required this.name, required this.description});
+}
 class Deck extends ElectronicModel {
-  Deck(
+  final int appCount;
+  late List<DeckApplication?> apps;
+  Deck(this.appCount,
       {required String name,
       required int rating,
       required int attack,
@@ -34,5 +51,14 @@ class Deck extends ElectronicModel {
                 attack: attack,
                 sleaze: sleaze,
                 dataProc: dataProc,
-                firewall: firewall));
+                firewall: firewall)) {
+    apps = List.generate(appCount, (index) => null);
+  }
+
+  void change(AttributeModel a, AttributeModel b)
+  {
+    int tmp = a.value;
+    a.value = b.value;
+    b.value = tmp;
+  }
 }

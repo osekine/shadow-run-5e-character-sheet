3cada2a0da42586fa846f501673478e67c8d6256
osekine <osekine42@gmail.com>
2024-05-03 14:17:23 +0300

    add some getters to ProtectModel

--- lib/model/protect_model.dart
+++ lib/model/protect_model.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'character_model.dart';
 
-class ProtectModel
-{
+class ProtectModel {
   final CharacterModel model;
   int _attrib = 0;
   int _armor = 0;
+  int get attrib => _attrib;
+  int get armor => _armor;
+  int get rawProtect => attrib + armor + bonus;
   int bonus = 0;
 
-  ProtectModel({required this.model})
-  {
+  ProtectModel({required this.model}) {
     _attrib = model.attributes.body.value;
     _armor = 0;
   }

173f211aca9a236989ad34b648b782dcfa43246b
neonLaserUnicorn <astrargot@gmail.com>
2024-05-22 12:46:40 +0300

    Add many spell subclasses

--- lib/model/magic_model.dart
+++ lib/model/magic_model.dart
@@ -6,7 +6,7 @@ enum SpellDamageType { physical, stun }
 
 enum SpellTime { instant, sustain, permanent }
 
-abstract class Spell {
+class Spell {
   final int drain;
   final String name;
   final String description;
@@ -24,4 +24,86 @@ abstract class Spell {
     required this.time,
     required this.damageType,
   });
+}
+
+enum CombatSpellModifier { direct, indirect, elemental }
+
+class CombatSpell extends Spell {
+  final List<CombatSpellModifier> modifiers;
+  CombatSpell(Spell spell, {required this.modifiers})
+      : super(
+            drain: spell.drain,
+            name: spell.name,
+            description: spell.description,
+            type: spell.type,
+            distance: spell.distance,
+            time: spell.time,
+            damageType: spell.damageType);
+}
+
+enum DetectionSpellModifier { active, passive }
+
+class DetectionSpell extends Spell {
+  final List<DetectionSpellModifier> modifiers;
+  DetectionSpell(Spell spell, {required this.modifiers})
+      : super(
+            drain: spell.drain,
+            name: spell.name,
+            description: spell.description,
+            type: spell.type,
+            distance: spell.distance,
+            time: spell.time,
+            damageType: spell.damageType);
+}
+
+enum HealthSpellModifier { essence }
+
+class HealthSpell extends Spell {
+  final List<HealthSpellModifier> modifiers;
+  HealthSpell(Spell spell, {required this.modifiers})
+      : super(
+            drain: spell.drain,
+            name: spell.name,
+            description: spell.description,
+            type: spell.type,
+            distance: spell.distance,
+            time: spell.time,
+            damageType: spell.damageType);
+}
+
+enum IllusionSpellModifier {
+  obvious,
+  realistic,
+  singleSense,
+  multiSense,
+  mana,
+  physical
+}
+
+class IllusionSpell extends Spell {
+  final List<IllusionSpellModifier> modifiers;
+  IllusionSpell(Spell spell, {required this.modifiers})
+      : super(
+            drain: spell.drain,
+            name: spell.name,
+            description: spell.description,
+            type: spell.type,
+            distance: spell.distance,
+            time: spell.time,
+            damageType: spell.damageType);
+}
+
+enum ManipulationSpellModifier { damaging, mental, physical, environmental }
+
+class ManipulationSpell extends Spell {
+  final List<ManipulationSpellModifier> modifiers;
+  ManipulationSpell(Spell spell, {required this.modifiers})
+      : super(
+            drain: spell.drain,
+            name: spell.name,
+            description: spell.description,
+            type: spell.type,
+            distance: spell.distance,
+            time: spell.time,
+            damageType: spell.damageType);
 }
